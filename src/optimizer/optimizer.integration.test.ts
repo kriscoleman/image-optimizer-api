304c28a31fac8fdaa818e5e117e2800becfc046f
Kris Coleman <kriscodeman@gmail.com>
2022-11-13 19:13:31 -0500

    green w/ integrations

---
+++ src/optimizer/optimizer.integration.test.ts
@@ -0,0 +1,45 @@
+import optimizer from './optimizer';
+
+let imageUrl: string;
+const tinyImageHttpUrl = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png';
+
+describe('optimizer and get-image-buffer-from-url integration', () => {
+  beforeEach(() => {
+    imageUrl = tinyImageHttpUrl;
+  });
+
+  it('reduces image size', async () => {
+    const results = await optimizer(imageUrl);
+
+    // note: if you go to the tinyImage url above and check the content-length of the response header for this image,
+    //       you'll notice it should be 3501.
+    //       we are checking if we actually reduce the image
+    const originalFileSize = 3501;
+    expect((await results?.buffer)?.byteLength).toBeLessThan(originalFileSize);
+
+    // note: now let's check that we are at our best.
+    //       I've set a regression benchmark, and we can check it here to make sure we're reducing the file as good,
+    //       or better, than we have before.
+
+    const regressionBenchmark = 2642;
+    expect((await results?.buffer)?.byteLength).toBeLessThanOrEqual(regressionBenchmark);
+  });
+
+  describe('validation', () => {
+    it('invalidates non image files', async () => {
+      imageUrl = 'https://nodejs.org/dist/v18.12.1/node-v18.12.1-darwin-x64.tar.gz';
+      expect(optimizer(imageUrl)).rejects.toThrow('invalid request');
+
+      imageUrl = 'https://nodejs.org/dist/v18.12.1/node-v18.12.1-darwin-x64.zip';
+      expect(optimizer(imageUrl)).rejects.toThrow('invalid request');
+
+      imageUrl = 'https://nodejs.org/dist/v18.12.1/node-v18.12.1-darwin-x64.msi';
+      expect(optimizer(imageUrl)).rejects.toThrow('invalid request');
+    });
+
+    it('invalidates non image dataUrls', async () => {
+      imageUrl = 'data:text/html,base64;ARUSL1ius90k';
+      expect(optimizer(imageUrl)).rejects.toThrow('invalid request');
+    });
+  });
+});
--- src/optimizer/optimizer.ts
+++ src/optimizer/optimizer.ts
@@ -1,17 +1,20 @@
 import sharp from 'sharp';
-import getImageBufferFromUrl from '../getImageBufferFromUrl/get-image-buffer-from-url';
+import getImageBufferFromUrl from '../get-image-buffer-from-url/get-image-buffer-from-url';
+import validator from '../validator/validator';
 
 const optimizer = async (imageUri: string) => {
   if (!imageUri) return;
+
+  if (!validator(imageUri)) throw new Error('invalid request');
 
   let image;
   if (imageUri.startsWith('http')) {
     image = await getImageBufferFromUrl(imageUri);
   } else {
-    image = new Buffer(imageUri.split(',')[1], 'base64');
+    image = Buffer.from(imageUri.split(',')[1], 'base64');
   }
 
-  const result = await sharp(image)
+  const result = sharp(image)
     .avif({ quality: 80, force: false })
     .jpeg({ quality: 80, progressive: true, force: false })
     .png({ quality: 80, progressive: true, force: false })
